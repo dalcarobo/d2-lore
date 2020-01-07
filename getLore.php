614e23b7c9183c86b1251cddf30c13d961dab1f0
Gabriel Dal Carobo <gabrieldolmayan@gmail.com>
2020-01-07 11:04:16 -0300

    Add files via upload

---
+++ getLore.php
@@ -0,0 +1,23 @@
+<?php
+//$db1 = new SQLite3('world_sql_content_e2e6f97ab386457d36c5c857d6b98fa3/world_sql_content_e2e6f97ab386457d36c5c857d6b98fa3.sqlite3');
+$db2 = new SQLite3('world_sql_content_e2e6f97ab386457d36c5c857d6b98fa3/world_sql_content_e2e6f97ab386457d36c5c857d6b98fa3.sqlite3');
+
+$results = $db2->query('select * from DestinyLoreDefinition');
+$m = array();
+while ($row = $results->fetchArray()) {
+  $lore = ($row['json']);
+  $a = (json_decode($lore));
+
+  $de = ['Derivante', 'Colméia', 'Despertos', 'Desperto'];
+  $para = ['Drifter', 'Hive', 'Awoken', 'Awoken'];
+
+  $n = str_replace($de, $para, $a->displayProperties->name);
+  $id = str_replace($de, $para, base64_encode($row['id']));
+
+  if ($n != "") {
+    $data = array("hash" => $a->hash, "nome" => $n, "id" => $id);
+    array_push($m, $data);
+  }
+}
+
+echo '{"data":' . json_encode($m) . ', "draw":"1","recordsTotal": 1, "recordsFiltered": 1}';
---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>Lorebook</title>
+
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+  <link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+</head>
+<body class="hold-transition layout-top-nav">
+<div class="wrapper">
+
+  <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
+    <div class="container">
+      <a href="index.php" class="navbar-brand">
+        <span class="brand-text font-weight-light">Lorebook</span>
+      </a>
+    </div>
+  </nav>
+
+  <div class="content-wrapper">
+    <div class="content-header">
+      <div class="container">
+        <div class="row mb-2">
+        </div>
+      </div>
+    </div>
+
+    <div class="content">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="card card-primary card-outline">
+              <div class="card-body">
+                <table id="table-lores" class="table table-hover">
+                  <thead>
+                  <tr>
+                    <th>#ID</th>
+                    <th>Título</th>
+                  </tr>
+                  </thead>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="plugins/jquery/jquery.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="dist/js/adminlte.min.js"></script>
+<script src="plugins/datatables/jquery.dataTables.js"></script>
+<script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+<script>
+    $(document).ready(function () {
+
+
+        const table = $('#table-lores').DataTable({
+            paging: true,
+            lengthChange: false,
+            searching: true,
+            responsive: true,
+            ordering: true,
+            info: true,
+            autoWidth: false,
+            "language": {
+                "url": "http://cdn.datatables.net/plug-ins/1.10.20/i18n/Portuguese-Brasil.json"
+            },
+            "ajax": {
+                "url": "getLore.php",
+                "type": "POST"
+            },
+            "columns": [
+              { 'data': 'hash' },
+              {
+                  'data': null,
+                  "render": function (a, b, c) {
+                      return "<a href='lore.php?uid="+c['id']+"'>"+c['nome']+"</a>";
+                  }
+              }
+             ]
+        });
+
+
+table.draw();
+
+        /*$.ajax({
+            url: 'getLore.php',
+            method: 'POST',
+            datatype: 'json',
+            success: function (data) {
+                let r = (JSON.parse(data));
+                console.log(r);
+                $('#table-lores').DataTable({
+                    data: r,
+                    columns: [
+                        { 'data': 'hash' },
+                        { 'data': 'nome' }
+                    ]
+                });
+            }
+        });*/
+
+    });
+</script>
+</body>
+</html>
